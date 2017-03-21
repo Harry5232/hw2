a142a20eba50ea6a97218f5f5956aff4a8a37e13
Harry5232 <harry5232@gmail.com>
2017-03-21 23:50:12 +0800

    3/21  1150

--- hw2_103753027.R
+++ hw2_103753027.R
@@ -1,46 +1,88 @@
 #作業2_new 完成版 103753027 顏碩亨
 
-#處理command下的變數
-#Rscript hw2_yourID.R --target male/female --files meth1 meth2 … methx
-#--out result.csv
-
+# read parameters
 args = commandArgs(trailingOnly=TRUE)
-
 if (length(args)==0) {
-  stop("USAGE: Rscript hw1_exam.R input", call.=FALSE)
-}else if(args[1] == '--files'){
-  
-  i <- 2
-  while(i){
-    
-    if(args[i] == '--out'){
-      i <- i + 1
-      break
-    }
-    f(args[i])
-    i <- i + 1
-  }
-  
-  wr(args[i], p)
-  
-   
-}else if(args[1] == '--out'){
-  
-  if(args[3] == "--files"){
-    i <- 4
-    while(i){
-      
-      if(is.na(args[i])){
-        wr(args[2], p)
-        break
-      }
-      f(args[i])
-      i <- i + 1
-      
-    }
-  }
-  
-}else {
-  stop("Rscript hw2_yourID.R --target male/female --files meth1 meth2 … methx --out result.csv", call.=FALSE)
+  stop("USAGE: Rscript hw1.R -query min|max -files file1 file2 ... filen –out out.csv", call.=FALSE)
 }
+
+# parse parameters
+i<-1 
+while(i < length(args))
+{
+  if(args[i] == "--target"){
+    query_m<-args[i+1]
+    i<-i+1
+  }else if(args[i] == "--files"){
+    j<-grep("-", c(args[(i+1):length(args)], "-"))[1]
+    files<-args[(i+1):(i+j-1)]
+    i<-i+j-1
+  }else if(args[i] == "--out"){
+    out_f<-args[i+1]
+    i<-i+1
+  }else{
+    stop(paste("Unknown flag", args[i]), call.=FALSE)
+  }
+  i<-i+1
+}
+
+print("PROCESS")
+print(paste("positive target :", query_m))
+print(paste("output file:", out_f))
+print(paste("files      :", files))
+
+# read files
+Meths <- c()
+Sens <- c() 
+Spes <- c()
+F1s <- c()
+AUC <- c()
+sens_max <- 0
+spec_max <- 0
+F1_max <- 0
+AUC_max <- 0
+
+for(file in files)
+{
+  TP <- 0
+  TN <- 0
+  FP <- 0
+  FN <- 0
+  Pre <- 0 #precision
+  Rec <- 0 #recall 
+  
+  d<-read.table(file, header=T,sep=",")
+  for(num in c(1:nrow(d))){
+    if(d[num,2] == d[num,3]){
+      if(d[num,3] == query_m){
+        TP <- TP + 1
+      }else{
+        TN <- TN + 1
+      }
+      
+    }else if(d[num,2] != d[num,3]){
+      if(d[num,3] == query_m){
+        FP <- FP + 1
+      }else{
+        FN <- FN + 1
+      }
+    }
+  }
+  Pre <- TP/(TP+FP)
+  Rec <- TP/(TP+FN)
+  F1 <- (2 * Pre * Rec)/(Pre + Rec)
+  Sen <- (TP/(TP + FN))
+  Spe <- (TN/(TN + FP))
+  
+  
+
+}
+out_data<-data.frame(meths, sens, F1, AUC, stringsAsFactors = F)
+
+
+
+# output file
+#最下面一列做結尾
+write.table(out_data, file=out_f, row.names = F, quote = F)
+
 #作業2_new 完成版 103753027 顏碩亨
