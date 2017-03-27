c142e64b03e0696340abe3f44f1cccf662c09fd5
Harry5232 <harry5232@gmail.com>
2017-03-27 21:00:12 +0800

    0327 2100

--- hw2_103753027.R
+++ hw2_103753027.R
@@ -1,88 +1,131 @@
 #作業2_new 完成版 103753027 顏碩亨
 
 # read parameters
+#library('ROCR')
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
-  stop("USAGE: Rscript hw1.R -query min|max -files file1 file2 ... filen –out out.csv", call.=FALSE)
-}
-
-# parse parameters
-i<-1 
-while(i < length(args))
-{
-  if(args[i] == "--target"){
-    query_m<-args[i+1]
-    i<-i+1
-  }else if(args[i] == "--files"){
-    j<-grep("-", c(args[(i+1):length(args)], "-"))[1]
-    files<-args[(i+1):(i+j-1)]
-    i<-i+j-1
-  }else if(args[i] == "--out"){
-    out_f<-args[i+1]
-    i<-i+1
-  }else{
-    stop(paste("Unknown flag", args[i]), call.=FALSE)
-  }
-  i<-i+1
-}
-
-print("PROCESS")
-print(paste("positive target :", query_m))
-print(paste("output file:", out_f))
-print(paste("files      :", files))
-
-# read files
-Meths <- c()
-Sens <- c() 
-Spes <- c()
-F1s <- c()
-AUC <- c()
-sens_max <- 0
-spec_max <- 0
-F1_max <- 0
-AUC_max <- 0
-
-for(file in files)
-{
-  TP <- 0
-  TN <- 0
-  FP <- 0
-  FN <- 0
-  Pre <- 0 #precision
-  Rec <- 0 #recall 
+  stop("", call.=FALSE)
+}else{
   
-  d<-read.table(file, header=T,sep=",")
-  for(num in c(1:nrow(d))){
-    if(d[num,2] == d[num,3]){
-      if(d[num,3] == query_m){
-        TP <- TP + 1
-      }else{
-        TN <- TN + 1
+  # parse parameters
+  i<-1 
+  while(i < length(args))
+  {
+    if(args[i] == "--target"){
+      query_m<-args[i+1]
+      i<-i+1
+    }else if(args[i] == "--files"){
+      j<-grep("--", c(args[(i+1):length(args)], "--"))[1]
+      files<-args[(i+1):(i+j-1)]
+      i<-i+j-1
+    }else if(args[i] == "--out"){
+      out_f<-args[i+1]
+      i<-i+1
+    }else{
+      stop(paste("Unknown flag", args[i]), call.=FALSE)
+    }
+    i<-i+1
+  }
+  
+  print("PROCESS")
+  print(paste("positive target :", query_m))
+  print(paste("output file:", out_f))
+  print(paste("files      :", files))
+  
+  # read files
+  method <- c()
+  sensitivity <- c() 
+  specificity <- c()
+  F1 <- c()
+  AUCs <- c()
+  sens_max <- list(num=0,name="")
+  spec_max <- list(num=0,name="")
+  F1_max <- list(num=0,name="")
+  AUC_max <- list(num=0,name="")
+  
+  for(file in files)
+  {
+    TP <- 0
+    TN <- 0
+    FP <- 0
+    FN <- 0
+    Pre <- 0 #precision
+    Rec <- 0 #recall 
+    AUC <- 0 #AUC
+    Pre.score <- c()
+    Pre.label <- c()
+    
+    d<-read.table(file, header=T,sep=",")
+    for(num in c(1:nrow(d))){
+      if(d[num,2] == d[num,3]){
+        if(d[num,3] == query_m){
+          TP <- TP + 1
+        }else{
+          TN <- TN + 1
+        }
+        
+      }else if(d[num,2] != d[num,3]){
+        if(d[num,3] == query_m){
+          FN <- FN + 1
+        }else{
+          FP <- FP + 1
+        }
       }
       
-    }else if(d[num,2] != d[num,3]){
-      if(d[num,3] == query_m){
-        FP <- FP + 1
-      }else{
-        FN <- FN + 1
-      }
     }
+    
+
+    Pre <- TP/(TP+FP)
+    Rec <- TP/(TP+FN)
+    f1 <- (2 * Pre * Rec)/(Pre + Rec)
+    f1 <- round(f1,digits = 2)
+    F1 <- c(F1,f1)
+    sen <- (TP/(TP + FN))
+    sen <- round(sen,digits = 2)
+    sensitivity <- c(sensitivity,sen)
+    Spe <- (TN/(TN + FP))
+    Spe <- round(Spe,digits = 2)
+    specificity <- c(specificity,Spe)
+    file <- strsplit(file,".csv")[[1]]
+    method <- c(method,file)
+    Pre.score <- c(d$pred.score)
+    Pre.label <- c(d$prediction)
+    #AUC <- auc(x=roc(Pre.score,0.5), min = 0, max = 1)
+    AUCs <- c(AUCs,AUC)
+    
+    
+    
+    
+    if(sen > sens_max$num){
+      sens_max$num <- sen
+      sens_max$name <- file
+    }
+   
+    if(Spe > spec_max$num){
+      spec_max$num <- Spe
+      spec_max$name <- file
+    }
+    
+    if(f1 > F1_max$num){
+      F1_max$num <- f1
+      F1_max$name <- file 
+    }
+    
+    if(AUC > AUC_max$num){
+      AUC_max$num <- AUC
+      AUC_max$name <- file
+    }
+    
   }
-  Pre <- TP/(TP+FP)
-  Rec <- TP/(TP+FN)
-  F1 <- (2 * Pre * Rec)/(Pre + Rec)
-  Sen <- (TP/(TP + FN))
-  Spe <- (TN/(TN + FP))
+  # output file
+  out_data<-data.frame(method, sensitivity, specificity, F1, AUC = AUCs, stringsAsFactors = F)
+  
+  #最下面一列做結尾
+  out_data[nrow(out_data)+1,] <- c("highest",sens_max$name,spec_max$name,F1_max$name,AUC_max$name)
+  
+  write.table(out_data, file=out_f, row.names = F, quote = F)
   
   
-
 }
-out_data<-data.frame(meths, sens, F1, AUC, stringsAsFactors = F)
-
-
-
-# output file
-#最下面一列做結尾
-write.table(out_data, file=out_f, row.names = F, quote = F)
 
 #作業2_new 完成版 103753027 顏碩亨
